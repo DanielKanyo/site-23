e607a825cc744809cb754ccd85164184fe7e720a
DanielKanyo <danielkanyog@gmail.com>
2022-06-16 09:57:11 +0200

    Scroll parallax added for About page

--- src/App/Components/About/About.jsx
+++ src/App/Components/About/About.jsx
@@ -1,16 +1,22 @@
+import { Parallax } from 'react-scroll-parallax';
+
 import './About.css';
 
 const About = () => {
     return (
         <div className='about' name='about'>
-            <div className='profile-img'></div>
-            <div className='about-text'>
-                With experience in web development and design,
-                I maintain a healthy balance between functionality and visual impact in all my work.
-                <br />
-                <br />
-                I believe every piece of design can be a work of art.
-            </div>
+            <Parallax translateY={[-25, 25]}>
+                <div className='profile-img'></div>
+            </Parallax>
+            <Parallax translateY={[25, -25]}>
+                <div className='about-text'>
+                    With experience in web development and design,
+                    I maintain a healthy balance between functionality and visual impact in all my work.
+                    <br />
+                    <br />
+                    I believe every piece of design can be a work of art.
+                </div>
+            </Parallax>
         </div>
     )
 }
