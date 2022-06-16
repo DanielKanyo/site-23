8841fc314e3519745c994febe96c35984f40218a
DanielKanyo <danielkanyo992@gmail.com>
2022-06-16 19:15:14 +0200

    Skills component

--- src/App/Components/Skills/Skills.jsx
+++ src/App/Components/Skills/Skills.jsx
@@ -1,29 +1,38 @@
+
+import { Parallax } from 'react-scroll-parallax';
+import { SKILLS } from '../../Static/Constants/skills';
+
 import './Skills.css';
 
 const Skills = () => {
-  return (
-    <div className='skills'>
-        <div className='title'>Skills</div>
-        <div className='content'>
-            <div className='skill-category-container right-align'>
-                <div className='skill-category-title'>
-                    Frontend
-                </div>
-                <div className='skill-category-list'>
-                    TODO: List of front-end skills comes here...
-                </div>
-            </div>
-            <div className='skill-category-container back-end'>
-                <div className='skill-category-title'>
-                    Backend
-                </div>
-                <div className='skill-category-list'>
-                    TODO: List of back-end skills comes here...
-                </div>
+    return (
+        <div className='skills'>
+            <div className='title'>Skills</div>
+            <div className='content'>
+                {
+                    SKILLS.map((stack) => (
+                        <Parallax translateX={stack.translateX} key={stack.stack} className='parallax'>
+                            <div className={`skill-category-container ${stack.rightAligned ? 'right-align' : ''}`}>
+                                <div className='skill-category-title'>
+                                    {stack.stack}
+                                </div>
+                                <div className={`skill-category-list ${stack.rightAligned ? 'right-flex' : 'left-flex'}`}>
+                                    {
+                                        stack.skills.map((skill) => (
+                                            <div className='skill-element' key={skill}>{skill}</div>
+                                        ))
+                                    }
+                                </div>
+                                <div className='and-more'>
+                                    and more...
+                                </div>
+                            </div>
+                        </Parallax>
+                    ))
+                }
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Skills;
---
+++ src/App/Static/Constants/skills.js
@@ -0,0 +1,20 @@
+export const SKILLS = [
+    {
+        stack: 'Frontend',
+        translateX: ['170px', '0px'],
+        skills: ['React', 'Angular', 'Polymer', 'D3.js', 'Lit', 'Bootstrap'],
+        rightAligned: true,
+    },
+    {
+        stack: 'Backend',
+        translateX: ['-170px', '0px'],
+        skills: ['Spring Boot', 'Node.js', 'Firebase', 'MongoDB'],
+        rightAligned: false,
+    },
+    {
+        stack: 'Other',
+        translateX: ['170px', '0px'],
+        skills: ['UX', 'Linux', 'Problem solving', 'Precision'],
+        rightAligned: true,
+    }
+]
