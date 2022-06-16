68ba25bf0ce1a0d9dedab08100b9248262ca807d
DanielKanyo <danielkanyog@gmail.com>
2022-06-16 12:17:03 +0200

    Giving title for the About section

--- src/App/Components/About/About.jsx
+++ src/App/Components/About/About.jsx
@@ -9,12 +9,15 @@ const About = () => {
                 <div className='profile-img'></div>
             </Parallax>
             <Parallax speed={10}>
-                <div className='about-text'>
-                    With experience in web development and design,
-                    I maintain a healthy balance between functionality and visual impact in all my work.
-                    <br />
-                    <br />
-                    I believe every piece of design can be a work of art.
+                <div className='about-text-container'>
+                    <div className='about-text-title'>ABOUT <span>ME</span></div>
+                    <div>
+                        With experience in web development and design,
+                        I maintain a healthy balance between functionality and visual impact in all my work.
+                    </div>
+                    <div>
+                        I believe every piece of design can be a work of art.
+                    </div>
                 </div>
             </Parallax>
         </div>
