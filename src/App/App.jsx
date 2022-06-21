dc104c648c523f4038eaaa3d6335b37a0cd28671
DanielKanyo <danielkanyo992@gmail.com>
2022-06-21 19:46:50 +0200

    Background gradient, footer component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,6 +3,7 @@ import Navigation from './Components/Navigation/Navigation';
 import About from './Components/About/About';
 import Skills from './Components/Skills/Skills';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <About />
             <Skills />
             <Contact />
+            <Footer />
         </div>
     );
 }
---
+++ src/App/Components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import IconButton from '@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { scroller } from 'react-scroll';
+import { SOCIAL } from '../../Static/Constants/social';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div>
+                {
+                    SOCIAL.map((s) =>
+                        <IconButton
+                            aria-label={s.label}
+                            size='large'
+                            className='social-icon'
+                            href={s.url}
+                            target='_blank'
+                            key={s.label}
+                        >
+                            {s.icon}
+                        </IconButton>
+                    )
+                }
+            </div>
+            <div>
+                Copyright © {new Date().getFullYear()}
+            </div>
+            <div>
+                <IconButton
+                    aria-label='share'
+                    className='share-icon'
+                    href='https://www.facebook.com/sharer/sharer.php?u=https%3A//danielkanyo.hu/'
+                    target='_blank'
+                >
+                    <ShareIcon fontSize='inherit' />
+                </IconButton>
+                <IconButton aria-label='top' className='arrow-icon' onClick={() => scroller.scrollTo('landing')}>
+                    <ArrowUpwardIcon fontSize='inherit' />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
