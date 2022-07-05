a80f7dd3e42fd97898c5336886a2906625fc43c3
DanielKanyo <danielkanyo992@gmail.com>
2022-07-05 17:16:08 +0200

    Css fixes

--- src/App/Components/Footer/Footer.jsx
+++ src/App/Components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
                     )
                 }
             </div>
-            <div>
+            <div className='copyright-txt'>
                 Copyright © {new Date().getFullYear()}
             </div>
             <div>
